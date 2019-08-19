6ade96e8a2b8f853bd02cf59918f78badc6cc72d
xing wang <uwspstar@gmail.com>
2019-08-19 14:55:26 -0500

    Update 00001-sort-3-unique-numbers.py

--- python/data-structures/00001-sort-3-unique-numbers.py
+++ python/data-structures/00001-sort-3-unique-numbers.py
@@ -9,6 +9,7 @@
 # print sortNums([3, 3, 2, 1, 3, 2, 1])
 # # [1, 1, 2, 2, 3, 3, 3]
 
+# solution 1 : space is o(n)
 def sortNums(nums):
   number_one = 0
   number_two = 0
@@ -21,6 +22,27 @@ def sortNums(nums):
     if x == 3:
       number_three += 1
   return [1] * number_one + [2] * number_two + [3] * number_three
+
+
+print(sortNums([3, 3, 2, 1, 3, 2, 1]))
+
+# solution 2 : space is o(1)
+def sortNums(nums):
+    num_one_index = 0 
+    num_three_index = len(nums) - 1
+    current_index = 0     
+    while current_index < num_three_index:  
+        if nums[current_index] == 1:
+          nums[current_index], nums[num_one_index] = nums[num_one_index], nums[current_index]
+          current_index += 1
+          num_one_index += 1
+        if nums[current_index] == 2:
+          current_index += 1
+        if nums[current_index] == 3:
+          nums[current_index], nums[num_three_index] = nums[num_three_index], nums[current_index] 
+          num_three_index -= 1  
+        print(current_index)
+    return nums
 
 
 print(sortNums([3, 3, 2, 1, 3, 2, 1]))
