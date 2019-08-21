55a84968762955d631095b6b1b492f5d86059194
xing wang <uwspstar@gmail.com>
2019-08-21 13:06:54 -0500

    Update 00003-single-Number.py

--- python/data-structures/00003-single-Number.py
+++ python/data-structures/00003-single-Number.py
@@ -5,6 +5,17 @@
 # Example:
 # Input: [4, 3, 2, 4, 1, 3, 2]
 
+-  with XOR 
+def singleNumber(nums):
+  unique = 0
+  for n in nums:
+    unique ^= n
+  return unique
+
+
+print singleNumber([4, 3, 2, 4, 1, 3, 2])
+
+- nest loop
 def singleNumber(nums):
   for i in range(len(nums)):
     no_found = True
@@ -14,3 +25,23 @@ def singleNumber(nums):
         break 
     if no_found:
       return nums[i]
+    
+    
+print singleNumber([4, 3, 2, 4, 1, 3, 2])
+
+- with dictory
+
+def singleNumber(nums):
+  my_dict = {}
+  for x in nums: 
+      if x in my_dict:
+        my_dict[x] = my_dict[x] + 1 
+      else:
+        my_dict[x] = 1
+  
+  print(f"my_dict : {my_dict}")
+  for key, val in my_dict.items():
+    if my_dict[key] == 1:
+      return key
+ 
+print(singleNumber([4, -1,3, 2, 4, 1, 1,3, 2]))
