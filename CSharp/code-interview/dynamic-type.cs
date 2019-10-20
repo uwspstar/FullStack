0c93acfcb2f8aa550b7542a1df74e1730b9c7c08
xing wang <uwspstar@gmail.com>
2019-10-20 18:46:03 -0500

    Create dynamic-type.cs

---
+++ CSharp/code-interview/dynamic-type.cs
@@ -0,0 +1,12 @@
+using System;
+public class Program
+    {
+        public static void Main(string[] args)
+        {
+            byte num = 100;
+            dynamic val = num;
+            Console.WriteLine(val.GetType());
+            val += 100;
+            Console.WriteLine(val.GetType());
+        }
+    }
