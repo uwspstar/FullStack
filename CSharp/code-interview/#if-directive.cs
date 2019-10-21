e172ef8b37fa123a420f93bcb0df8af593bddfd9
xing wang <uwspstar@gmail.com>
2019-10-20 19:12:52 -0500

    Update #if-directive.cs

--- CSharp/code-interview/#if-directive.cs
+++ CSharp/code-interview/#if-directive.cs
@@ -1,3 +1,13 @@
+/*
+https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/preprocessor-directives/preprocessor-if
+
+You can use the operators == (equality) and != (inequality) only to test for true or false. 
+True means the symbol is defined. 
+The statement #if DEBUG has the same meaning as #if (DEBUG == true). 
+You can use the operators && (and), || (or), 
+and ! (not) to evaluate whether multiple symbols have been defined. 
+You can also group symbols and operators with parentheses.
+*/
 using System;
 public class Program
     {
