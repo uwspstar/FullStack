1ce85dba15464e76d395c6e955d993c347c36867
xing wang <uwspstar@gmail.com>
2019-10-22 05:15:38 -0500

    Create create-obj-null.cs

---
+++ CSharp/code-interview/create-obj-null.cs
@@ -0,0 +1,19 @@
+using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Program obj = null;
+        Console.WriteLine(Program.print());
+    }
+    private static String print()
+    {
+        return "Hi, I am a Tech-savvy!!";
+    }
+}
+
+/*
+main.cs(6,21): warning CS0219: The variable `obj' is assigned but its value is never used
+Compilation succeeded - 1 warning(s)
+*/
+// Hi, I am a Tech-savvy!!
