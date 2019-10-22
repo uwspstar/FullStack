deef510fd3926d0f6fbe460fec3b51db315640ee
xing wang <uwspstar@gmail.com>
2019-10-22 05:21:25 -0500

    Update create-obj-null.cs

--- CSharp/code-interview/create-obj-null.cs
+++ CSharp/code-interview/create-obj-null.cs
@@ -17,3 +17,9 @@ main.cs(6,21): warning CS0219: The variable `obj' is assigned but its value is n
 Compilation succeeded - 1 warning(s)
 */
 // Hi, I am a Tech-savvy!!
+/*
+The null keyword is a literal that represents a null reference, 
+one that does not refer to any object. 
+null is the default value of reference-type variables.
+
+*/
