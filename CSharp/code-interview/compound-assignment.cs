8d6014f965cb803fdb0a00b475456e669c96f60e
xing wang <uwspstar@gmail.com>
2019-10-23 08:24:59 -0500

    Update compound-assignment.cs

--- CSharp/code-interview/compound-assignment.cs
+++ CSharp/code-interview/compound-assignment.cs
@@ -9,3 +9,23 @@ Console.WriteLine(test);  // output: True
 
 test ^= false;
 Console.WriteLine(test);  // output: True
+
+
+using System;
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+        int num1 = 20;
+        int num2 = 30;
+
+        Console.WriteLine(num1 ^= num2); // 10
+        Console.WriteLine(num2 ^= num1); // 20
+        Console.WriteLine(num1 ^= num2); // 30
+        Console.WriteLine(num1 + ","+ num2); //30, 20
+
+        num1 ^= num2 ^= num1 ^= num2;
+        Console.WriteLine(num1 + ","+ num2); //0, 30
+    }
+}
