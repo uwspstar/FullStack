6ad573b2ff74c525334ff06eac0b944d10442003
xing wang <uwspstar@gmail.com>
2019-10-24 21:24:23 -0500

    Create new-list.cs

---
+++ CSharp/code-interview/new-list.cs
@@ -0,0 +1,20 @@
+/*
+There is no List, there is a generic class named List<T>, 
+requires a type parameter.
+You can't create a generic list without specifying the type parameter.
+*/
+
+using System;
+using System.Collections.Generic;
+public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var actions = new List();
+            for (int i = 0; i < 4; i++) actions.Add(() => Console.WriteLine(i));
+            foreach (var action in actions)
+                action();
+        }
+    }
+    
+   //  Using the generic type `System.Collections.Generic.List<T>' requires `1' type argument(s)
