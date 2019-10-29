4b803ddd18501f54fb8bd176280dddfb7120d988
xing wang <uwspstar@gmail.com>
2019-10-29 15:54:58 -0500

    Update find-word-inside-file.cs

--- CSharp/code-interview/find-word-inside-file.cs
+++ CSharp/code-interview/find-word-inside-file.cs
@@ -1,1 +1,6 @@
-//  find how many time the word repeat inside a file
+// 1) find how many time the word repeat inside a file
+// 2) gabage collection
+// 3) performance issue encount
+// 4) SOLID
+// 5) Depended Injection
+// 6) Sigleton
