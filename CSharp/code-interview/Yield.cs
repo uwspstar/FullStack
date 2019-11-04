3169053a8c6e71f0d1fcbc40bf7dc7015560a620
xing wang <uwspstar@gmail.com>
2019-11-03 19:12:15 -0600

    Create Yield.cs

---
+++ CSharp/code-interview/Yield.cs
@@ -0,0 +1,22 @@
+private static void ListEvenNumbers()
+{
+    foreach (int number in EvenSequence(5, 18))
+    {
+        Console.Write(number.ToString() + " ");
+    }
+    Console.WriteLine();
+    // Output: 6 8 10 12 14 16 18
+}
+
+private static IEnumerable<int> EvenSequence(
+    int firstNumber, int lastNumber)
+{
+    // Yield even numbers in the range.
+    for (var number = firstNumber; number <= lastNumber; number++)
+    {
+        if (number % 2 == 0)
+        {
+            yield return number;
+        }
+    }
+}
