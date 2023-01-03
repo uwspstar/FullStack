f9fa7c5d4a310bde2ede9013143ac428649a18e3
xing wang <uwspstar@gmail.com>
2023-01-03 07:56:30 -0600

    daily

--- python/Learn-Python-from-Leetcode/set-00217. Contains Duplicate.py
+++ python/Learn-Python-from-Leetcode/set-00217. Contains Duplicate.py
@@ -1,3 +1,17 @@
+"""
+Loop Invariant, Linear Search, Sorting and Hash Table.
+
+when n is not sufficiently large, an O(n)O algorithm can be slower than an O(nlogn) algorithm.
+
+(class) set()
+
+set(__iterable: Iterable, /)
+
+set() -> new empty set object set(iterable) -> new set object
+
+Build an unordered collection of unique elements.
+"""
+
 from typing import List
 
 # s1 t: O(N) s: O(N)
@@ -19,13 +33,4 @@ sl = Solution()
 print(sl.contains_duplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]))  # true
 print(sl.contains_duplicate([1, 3, 4, 2]))  # false
 
-"""
-(class) set()
-
-set(__iterable: Iterable, /)
-
-set() -> new empty set object set(iterable) -> new set object
-
-Build an unordered collection of unique elements.
-"""
 # s2, sorting : O(NlogN)
